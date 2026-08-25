2c59816e23a3b951c91b0c8d27a51952505030a2
besttech44 <mohammedkolsawala81@gmail.com>
2026-08-25 11:48:53 +0530

    Create room_heater.py

---
+++ room_heater.py
@@ -0,0 +1,12 @@
+#regulating use of room heater...
+
+temperature=input("What is the temperature right now?  ")
+
+#if else block
+
+if int(temperature)<25:
+    print("It is cold right now....")
+    print("Turn the room heater on.")
+else:
+    print("It is not so cold right now....")
+    print("Turn the room heater off.")
