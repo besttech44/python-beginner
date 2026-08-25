6ba54b1854391319af472b16308fd3716c75dae4
besttech44 <mohammedkolsawala81@gmail.com>
2026-08-25 09:49:53 +0530

    Create messager.py

---
+++ messager.py
@@ -0,0 +1,7 @@
+# My Python Journey - Started Aug 2026 from Surat
+Day 1: Messager search app
+- Learned: string vs int bug, loops
+
+message = input("Search the message:")
+friend_name = input("Type the friend's name of whom you want to search messages:")
+print(friend_name + " says " + message)
